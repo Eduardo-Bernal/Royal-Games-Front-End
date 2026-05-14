08a7b5f9b17c18dd528d98e1d8158725a2c6d84f
Eduardo-Bernal <cbedudu@gmail.com>
2026-05-14 10:44:43 -0300

    login

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,34 @@
+import styles from "./login.module.css"
+import Btn from "../../components/btn/btn";
+import {useState} from "react";
+
+const Login = () => {
+
+
+    return (
+        <>
+            <main className={styles.main}>
+                <div className={styles.imagem_lateral}>
+                    <img src="/imgs/mulher%20login.png" alt=""/>
+                </div>
+                <div className={styles.form_login}>
+                    <img className={styles.logo} src="/imgs/Logo%20Header.png" alt=""/>
+                    <form className={styles.campos_login} action="">
+                        <div className={styles.campo_texto}>
+                            <label htmlFor="">Email</label>
+                            <input type="text"/>
+                        </div>
+                        <div className={styles.campo_texto}>
+                            <label htmlFor="">Senha</label>
+                            <input type="password"/>
+                        </div>
+                        <Btn>Entrar</Btn>
+                    </form>
+                </div>
+
+            </main>
+        </>
+    )
+}
+
+export default Login
